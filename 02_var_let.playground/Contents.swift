b4590dc41c8cdadb44c7fa7592d2e9d6200b0b2a
David Altamirano <davidaltamiranodev@gmail.com>
2024-01-22 16:50:27 -0500

    Se crea el fichero let y var donde se abordan las declaraciones y asignaciones de variables y constantes en swift.

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let maximumNumberOfLoginAttempts = 3 // Sintaxis para declarar constantes en swift
+
+var currentLoginAttempts = 0 // Sintaxis para declarar variables en swift
+
+var x = 0.0, y = 0.0, z = 0.0 // Se pueden declarar variables en una sola linea
+
+var welcomeMessage : String
+
+welcomeMessage = "Hola, que tal?"
+
+var red, green, blue : Double
+
+let π = 3.141592 // option + p sirve para escribir el número pi
+let 👽 = "Alien" // Se pueden usar emojis para definir variables o constantes
+
+print(👽) // Alien debe ser su salida
+
+print("El número de login actual es: \(currentLoginAttempts) de un total disponible de \(maximumNumberOfLoginAttempts)")
+
+// 
