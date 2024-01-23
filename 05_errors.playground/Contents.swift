842fe0337d0d8d2d6000370ceeb9b1a25488e109
David Altamirano <davidaltamiranodev@gmail.com>
2024-01-23 17:02:00 -0500

    Se crea el fichero errors para tratar el manejo de errores mediante el uso de try, catch, throws, aserciones y precondiciones en swift.

--- 05_errors.playground/Contents.swift
+++ 05_errors.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func canThrowError() throws{
+    // Aquí hay codigo que puede causar un error
+}
+
+do {
+    try canThrowError()
+    // Si llegamos aqui, no ha habido error
+}catch{
+    // Si llegamos aqui, ha habido un error...
+}
+
+
+func makeASandwich() throws{
+    
+}
+
+do {
+    try makeASandwich()
+    // Me como el sandwich
+}catch{
+    // tengo platos limpios... -> lavar los platos
+    // Tengo ingredientes... -> deberia comprar ingredientes
+    // Tengo hambre... ->
+}
+
+
+// Aserciones (debug) y precondiciones (buil) produccion
+
+let age = -5
+//assert(age>=0, "La edad de una persona no puede ser negativa")
+precondition(age>=0, "La edad de una persona no puede ser negativa")
+// ... aqui el codigo sigue
+
+if age > 10{
+    print("Puedes subir a la montańa rusa")
+}else if age >= 0{
+    print("Eres demasiado pequeńo para subir a la montańa rusa")
+}else{
+    assertionFailure("Eres demasiado pequeńo para subir a la montańa rusa")
+}
+
