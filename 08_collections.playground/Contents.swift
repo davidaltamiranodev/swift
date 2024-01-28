98ebbd41aba7c463d32da8c7ad054e60754ac38e
David Altamirano <davidaltamiranodev@gmail.com>
2024-01-28 13:48:51 -0500

    Se actualiza el fichero collections con las principales coleccioes en swift.

--- 08_collections.playground/Contents.swift
+++ 08_collections.playground/Contents.swift
@@ -63,4 +63,115 @@ for(idx, item) in shoppingList.enumerated(){
 
 
 // Conjunto(Set)
+var letters = Set<Character>()
+letters.count
+letters.insert("a")
+letters.insert("h")
+letters.insert("b")
+
+//var favouriteGames: Set<String> = []
+//favouriteGames.count
+
+var favouriteGames : Set<String> = ["Final Fantasy", "World of Warcraft", "Farcry"]
+favouriteGames.count
+
+if favouriteGames.isEmpty{
+    print("No hay juegos favoritos")
+}
+
+favouriteGames.insert("Metal Gear")
+
+if let removedGame = favouriteGames.remove("Farcry"){
+    print("He eliminado de la lista \(removedGame)")
+}else{
+    print("no ha sido posible eliminar de la lista")
+}
+
+if favouriteGames.contains("Metal Gear"){
+    print("Me encanta ese juego")
+}
+
+for vg in favouriteGames.sorted() {
+    print(vg)
+}
+
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
+let primeNumbers: Set = [2, 3, 5, 7]
+
+// A union B = elementos que son o bien de A, o bien de B o de los dos
+oddDigits.union(evenDigits).sorted()
+// A interseccion B = elementos que son a la vez de A y de B
+oddDigits.intersection(evenDigits)
+evenDigits.intersection(primeNumbers).sorted()
+oddDigits.intersection(primeNumbers).sorted()
+
+// A - B = elementos que son de A pero no de B
+oddDigits.subtracting(primeNumbers).sorted()
+
+// A + B = (A - B) union (B - A)
+oddDigits.symmetricDifference(primeNumbers).sorted()
+
+let houseAnimals: Set = ["🐶", "🐱"]
+let farmAnimals : Set = ["🐮", "🐔", "🐑", "🐶", "🐱"]
+let cityAnimals : Set = ["🐀", "🕊️"]
+
+houseAnimals.isSubset(of: farmAnimals)
+farmAnimals.isSuperset(of: houseAnimals)
+// A y B son disjuntos si su interseccion es vacía
+farmAnimals.isDisjoint(with: cityAnimals)
+
+
+// Diccionarios [k1:v1, k2:v2, ...]
+
+var nameOfIntegers = [Int : String]()
+nameOfIntegers[15] = "quince"
+nameOfIntegers = [:]
+
+var airports: [String: String] = ["YYZ": "Toronto",
+                                  "DUB": "Dublin",
+                                  "PMI": "Palma de Mallorca"
+]
+
+airports.count
+
+airports.isEmpty
+airports["LHR"] = "London City Airport"
+airports
+airports["LHR"] = "London Heathrow"
+airports
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB"){
+    print("El aeropuerto tenia antiguamente el nombre de \(oldValue)")
+}
+airports
+
+if let airportName = airports["DUB"]{
+    print("El aeropuerto de DUB es: \(airportName)")
+}
+
+airports["PMI"] = nil
+airports
+
+if let removeAirports = airports.removeValue(forKey: "DUB"){
+    // usar la variable removed airport
+}
+
+airports
+
+for (key, value) in airports{
+    print("\(key) - \(value)")
+}
+
+for airportKey in airports.keys{
+    print(airportKey)
+}
+
+for airportName in airports.values{
+    print(airportName)
+}
+
+let airportKeys = [String](airports.keys.sorted())
+let airportNames = [String](airports.values.sorted())
+
 
