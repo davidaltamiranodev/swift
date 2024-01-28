abb351db622af058f1f3438066f2ad169011a5e6
David Altamirano <davidaltamiranodev@gmail.com>
2024-01-28 13:49:34 -0500

    Se crea el fichero control flow para tratar las principales estructuras de control en swift.

--- 09_control_flow.playground/Contents.swift
+++ 09_control_flow.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let names = ["Ricardo Celis", "Juan Gabriel", "Edgar"]
+
+// Bucle For
+
+for name in names {
+    print("Hola \(name)")
+}
+
+
+let numberOfLegs = ["spider": 8, "ant": 6, "dog": 4]
+
+for (animalName, legCount) in numberOfLegs{
+    print("Animal: \(animalName): número de patas: \(legCount)")
+}
+
+for idx in 1...5{
+    print("\(idx) x 3 = \(idx * 3)")
+}
+
+let base = 2
+let power = 10
+var answer = 1
+
+for _ in 0..<power{
+    answer *= base
+    print(answer)
+}
+
+var hour = 9
+let minutes = 60
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval){
+    print("\(hour) : \(tickMark)")
+}
+
+// Bucle While
+
+var i = 0
+while i <= 10{
+    print(i)
+    i += 1
+}
+
+repeat {
+    print(i)
+    i += 1
+} while i <= 20
+
+
